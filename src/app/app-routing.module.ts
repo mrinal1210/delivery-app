fc48b7f3bd7908ee69aee48df617d6438a7649e2
mrinal1210 <mrinal1012kishore@gmail.com>
2021-09-17 13:10:02 +0530

    Created the sign up page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,7 +18,11 @@ const routes: Routes = [
   {
     path: 'forgot-password',
     loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
-  }
+  },
+  {
+    path: 'signup',
+    loadChildren: () => import('./signup/signup.module').then( m => m.SignupPageModule)
+  },
 
 ];
 
