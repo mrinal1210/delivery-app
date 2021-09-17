9dcc2ef8b4bf71f478f1b98d32df019e04f2f8b9
mrinal1210 <mrinal1012kishore@gmail.com>
2021-09-17 13:09:00 +0530

    Created forgot password page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,10 +10,15 @@ const routes: Routes = [
     path: '',
     redirectTo: 'home',
     pathMatch: 'full'
-  },  {
+  },
+  {
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'forgot-password',
+    loadChildren: () => import('./forgot-password/forgot-password.module').then( m => m.ForgotPasswordPageModule)
+  }
 
 ];
 
