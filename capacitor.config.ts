de45721191a0ebb520d00edc389cf93126624281
mrinal1210 <mrinal1012kishore@gmail.com>
2021-09-17 13:06:56 +0530

    Added '@capacitor/core' & '@capacitor/cli' plugin

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mrinal.deliveryApp',
+  appName: 'DeliveryApp',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
